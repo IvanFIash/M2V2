bfaef0d10b21d463987376870d8877efe78060e0
ivan <ivanams2004@gmail.com>
2024-11-28 04:35:56 -0600

    Codigo prueba MPU6050

--- main_algorithm/ALEDs/leds.py
+++ main_algorithm/ALEDs/leds.py
@@ -1,11 +1,15 @@
 import board
 import neopixel
+import sys
+
+# Recibir los argumentos de la línea de comandos
+arr = sys.argv
 
 # Configuración de NeoPixel (pines y cantidad de LEDs)
 pixels = neopixel.NeoPixel(board.D18, 10, brightness=0.5, pixel_order=neopixel.GRBW)
 
-# Encender un LED con color rojo puro
-pixels[0] = (255, 0, 0, 0)  # (R, G, B, W)
-
-# Encender un LED con blanco cálido
-pixels[1] = (0, 0, 255, 0)
+for i in range(len(arr)):
+    if arr[i] == 1:
+        pixels[i] = (0, 0, 0, 255)
+    else:
+        pixels[i] = (0, 0, 0, 0)
