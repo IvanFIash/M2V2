85048fefed87de6d6210e33c5e6423ce6c824266
ivan <ivanams2004@gmail.com>
2024-11-28 17:09:12 -0600

    Codigo prueba MPU9250 con Camara y Perspectiva

---
+++ main_algorithm/Mpu6050_Cam/leds.py
@@ -0,0 +1,16 @@
+import board
+import neopixel
+import sys
+
+# Recibir los argumentos de la línea de comandos
+arr = sys.argv
+arr.pop(0)
+
+# Configuración de NeoPixel (pines y cantidad de LEDs)
+pixels = neopixel.NeoPixel(board.D18, 10, brightness=0.5, pixel_order=neopixel.GRBW)
+
+for i in range(len(arr)):
+    if arr[i] == '1':
+        pixels[i] = (0, 0, 0, 255)
+    else:
+        pixels[i] = (0, 0, 0, 0)
