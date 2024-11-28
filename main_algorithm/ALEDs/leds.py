1803bbbac15cfad7557f508b31a6ff095bf84976
ivan <ivanams2004@gmail.com>
2024-11-28 04:42:29 -0600

    Codigo prueba MPU6050

--- main_algorithm/ALEDs/leds.py
+++ main_algorithm/ALEDs/leds.py
@@ -9,10 +9,8 @@ arr.pop(0)
 # Configuración de NeoPixel (pines y cantidad de LEDs)
 pixels = neopixel.NeoPixel(board.D18, 10, brightness=0.5, pixel_order=neopixel.GRBW)
 
-print(arr)
-
 for i in range(len(arr)):
-    if arr[i] == 1:
+    if arr[i] == '1':
         pixels[i] = (0, 0, 0, 255)
     else:
         pixels[i] = (0, 0, 0, 0)
