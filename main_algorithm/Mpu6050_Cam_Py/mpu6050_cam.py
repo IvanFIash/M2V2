8425a04c968ac046c46e84728d0eda2597d1e7ef
ivan <ivanams2004@gmail.com>
2024-11-28 00:52:29 -0600

    Codigo prueba MPU6050

--- main_algorithm/Mpu6050_Cam_Py/mpu6050_cam.py
+++ main_algorithm/Mpu6050_Cam_Py/mpu6050_cam.py
@@ -1,5 +1,6 @@
 from picamera2 import Picamera2
 import subprocess
+import time
 
 def get_angle_from_accelerometer():
     result = subprocess.run(["./mpu6050-kalman"], capture_output=True, text=True)
@@ -9,14 +10,18 @@ def get_angle_from_accelerometer():
 picam2 = Picamera2()
 picam2.configure(picam2.create_still_configuration())
 
-# Capturar la imagen y calcular el ángulo
-picam2.start()
-image = picam2.capture_file("photo.jpg")  # Capturar y guardar la foto
-picam2.stop()
+for i in range(10):
+    start = time.time()
+    # Capturar la imagen y calcular el ángulo
+    picam2.start()
+    image = picam2.capture_file("photo.jpg")  # Capturar y guardar la foto
+    picam2.stop()
 
-print("Foto tomada y guardada como 'photo.jpg'.")
+    print("Foto tomada y guardada como 'photo.jpg'.")
 
-# Calcular el ángulo usando el acelerómetro
-angles = get_angle_from_accelerometer()
+    # Calcular el ángulo usando el acelerómetro
+    angles = get_angle_from_accelerometer()
 
-print(f"Roll: {float(angles[0])}, Pitch: {float(angles[1])}")
+    print(f"Roll: {float(angles[0])}, Pitch: {float(angles[1])}")
+    end = time.time()
+    print(end - start)
