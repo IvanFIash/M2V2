625e7c83ae51c3a2c46db67e9a9a4c530545a559
ivan <ivanams2004@gmail.com>
2024-11-28 09:10:13 -0600

    Codigo prueba MPU9250 con Camara

--- main_algorithm/Mpu6050_Cam/Mpu6050_Cam.c
+++ main_algorithm/Mpu6050_Cam/Mpu6050_Cam.c
@@ -344,7 +344,7 @@ int main()
     }
 
     t = clock() - t;
-    double time_taken = ((double)t)/CLOCKS_PER_SEC;
+    time_taken = ((double)t)/CLOCKS_PER_SEC;
     printf("Tardo %f segundos en guardar la imgajen.\n", time_taken);
 
     fwrite(buffer, buf.bytesused, 1, file);
