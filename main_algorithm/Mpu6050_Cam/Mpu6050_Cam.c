972fab2ef2b497abc558bb0e15f6a47817bb1a55
ivan <ivanams2004@gmail.com>
2024-12-01 10:48:59 -0600

    Codigo prueba MPU9250 con Camara y Perspectiva

--- main_algorithm/Mpu6050_Cam/Mpu6050_Cam.c
+++ main_algorithm/Mpu6050_Cam/Mpu6050_Cam.c
@@ -609,8 +609,8 @@ int main()
         printf("FPS: %f.\n", 1/(time_taken));
     }
 
-    printf("Se guardo la imagen con ancho: %dpx, alto: %dpx\n", f_width, f_height);
-    stbi_write_jpg("cpimg.jpg", f_width, f_height, 1, ptimg, 100);
+    //printf("Se guardo la imagen con ancho: %dpx, alto: %dpx\n", f_width, f_height);
+    //stbi_write_jpg("cpimg.jpg", f_width, f_height, 1, ptimg, 100);
 
     printf("No lane detected");
 
